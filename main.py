00f691a3630113a3de1779497b5caadfdc31b1c8
Veronika <vkotsubiak@gmail.com>
2022-10-28 19:46:41 +0300

    class Diploma

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ class Student:
         self.gradebook = [5, 6, 12, 8, 1, 11, 10]
         self.gradebookplus = sum(self.gradebook) / len(self.gradebook)
 
+class Diploma:
+    def __init__(self):
+        self.diplom = "Диплом"
+    def diploma(self):
+        if self.gradebookplus > 5:
+            print("Я захистив диплом")
+        else:
+            print("Я не захистив диплом, я пішов на перездачу.")
+
+
+    def method(self):
+        print("about Aspirant")
+
+
